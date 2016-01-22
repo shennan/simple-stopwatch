af6b4ddf902d58a050b51671f387abbf7fbf8154
shennan <jackotisbarker@gmail.com>
2016-01-22 01:19:02 +0000

    formatting of code

--- index.js
+++ index.js
@@ -15,184 +15,184 @@
 
   function SimpleStopwatch (dur, res, fmt) {
 
-  	/* private vars */
+    /* private vars */
 
-  	var self = this;
+    var self = this;
 
-  	var resolution = res || 1000;
-  	var duration = dur || 0;
-  	var format = fmt || '%m:%s:%ms';
+    var resolution = res || 1000;
+    var duration = dur || 0;
+    var format = fmt || '%m:%s:%ms';
 
-  	var milliseconds;
-  	var started;
-  	var timeout;
+    var milliseconds;
+    var started;
+    var timeout;
 
-  	var events = {};
+    var events = {};
 
-  	/* public methods */
+    /* public methods */
 
-  	function start () {
+    function start () {
 
-  		started = new Date().getTime();
+      started = new Date().getTime();
 
-  		if (milliseconds)
-  			tick(resolution - (milliseconds % resolution));
-  		else
-  			tick();
+      if (milliseconds)
+        tick(resolution - (milliseconds % resolution));
+      else
+        tick();
 
-  		emit('start');
+      emit('start');
 
-  		return self;
+      return self;
 
-  	}
+    }
 
-  	function stop () {
+    function stop () {
 
-  		untick();
+      untick();
 
-  		emit('stop');
+      emit('stop');
 
-  		return self;
+      return self;
 
-  	}
+    }
 
-  	function reset () {
+    function reset () {
 
-  		untick();
+      untick();
 
-  		started = undefined;
-  		milliseconds = 0;
+      started = undefined;
+      milliseconds = 0;
 
-  		emit('reset');
+      emit('reset');
 
-  		return self;
+      return self;
 
-  	}
+    }
 
-  	function on (event, handler) {
+    function on (event, handler) {
 
-  		if (!events[event])
-  			events[event] = [handler];
-  		else
-  			events[event].push(handler);
+      if (!events[event])
+        events[event] = [handler];
+      else
+        events[event].push(handler);
 
-  		return self;
+      return self;
 
-  	}
+    }
 
-  	function off (event, handler) {
+    function off (event, handler) {
 
-  		if (events[event]) {
+      if (events[event]) {
 
-  			for (var i = 0; i < events[event].length; i++) {
+        for (var i = 0; i < events[event].length; i++) {
 
-  				if (events[event][i] == handler)
-  					events[event].splice(i, 1),
-  					i--;
+          if (events[event][i] == handler)
+            events[event].splice(i, 1),
+            i--;
 
-  			}
-  		}
+        }
+      }
 
-  		return self;
+      return self;
 
-  	}
+    }
 
-  	function current () {
+    function current () {
 
-  		if (timeout) {
+      if (timeout) {
 
-  			return new Date().getTime() - started + (milliseconds || 0);
+        return new Date().getTime() - started + (milliseconds || 0);
 
-  		} else {
+      } else {
 
-  			return milliseconds || 0;
+        return milliseconds || 0;
 
-  		}
-  	}
+      }
+    }
 
-  	function time (mill) {
+    function time (mill) {
 
-  		var c = typeof mill == 'undefined' ? current() : mill;
+      var c = typeof mill == 'undefined' ? current() : mill;
 
-  		var ms = Math.floor(((c % 1000) / 1000) * 100);
-			var s = (c - (c %= 1000)) / 1000;
-			var m = (s - (s %= 60)) / 60;
-			var h = (m - (m %= 60)) / 60;
+      var ms = Math.floor(((c % 1000) / 1000) * 100);
+      var s = (c - (c %= 1000)) / 1000;
+      var m = (s - (s %= 60)) / 60;
+      var h = (m - (m %= 60)) / 60;
 
-			ms = ms < 10 ? '0' + ms : ms;
-			s = s < 10 ? '0' + s : s;
-			m = m < 10 ? '0' + m : m;
-			h = h < 10 ? '0' + h : h;
+      ms = ms < 10 ? '0' + ms : ms;
+      s = s < 10 ? '0' + s : s;
+      m = m < 10 ? '0' + m : m;
+      h = h < 10 ? '0' + h : h;
 
-			return format.replace(/%ms/g, ms).replace(/%s/g, s).replace(/%m/g, m).replace(/%h/g, h);
+      return format.replace(/%ms/g, ms).replace(/%s/g, s).replace(/%m/g, m).replace(/%h/g, h);
 
-  	}
+    }
 
-  	/* private methods */
+    /* private methods */
 
-  	function emit (event, args) {
+    function emit (event, args) {
 
-  		if (events[event]) {
+      if (events[event]) {
 
-  			for (var i = 0; i < events[event].length; i++) {
+        for (var i = 0; i < events[event].length; i++) {
 
-  				events[event][i].apply(self, args);
+          events[event][i].apply(self, args);
 
-  			}
-  		}
-  	}
+        }
+      }
+    }
 
-  	function tick (remaining) {
+    function tick (remaining) {
 
-  		var c = current();
-  		var next = remaining || resolution;
+      var c = current();
+      var next = remaining || resolution;
 
-  		emit('tick', [time(c)]);
+      emit('tick', [time(c)]);
 
-  		if (!duration || c + next < duration)
-  			timeout = setTimeout(tick, next);
-  		else
-  			timeout = setTimeout(dong, duration - c);
+      if (!duration || c + next < duration)
+        timeout = setTimeout(tick, next);
+      else
+        timeout = setTimeout(dong, duration - c);
 
-  	}
+    }
 
-  	function untick () {
+    function untick () {
 
-  		milliseconds = current();
+      milliseconds = current();
 
-  		if(timeout)
-  			clearTimeout(timeout),
-  			timeout = undefined;
+      if(timeout)
+        clearTimeout(timeout),
+        timeout = undefined;
 
-  	}
+    }
 
-  	function dong () {
+    function dong () {
 
-  		milliseconds = duration;
+      milliseconds = duration;
 
-  		if(timeout)
-  			clearTimeout(timeout),
-  			timeout = undefined;
+      if(timeout)
+        clearTimeout(timeout),
+        timeout = undefined;
 
-  		emit('dong', [duration]);
+      emit('dong', [duration]);
 
-  	}
+    }
 
-  	/* expose public methods */
+    /* expose public methods */
 
-  	self.start = start;
-  	self.stop = stop;
-  	self.reset = reset;
-  	self.on = on;
-  	self.off = off;
-  	self.time = time;
-  	self.current = current;
-  	
-  	self.duration = function (dur) { duration = dur; }
-  	self.resolution = function (res) { resolution = res; }
-  	self.format = function (fmt) { format = fmt; }
+    self.start = start;
+    self.stop = stop;
+    self.reset = reset;
+    self.on = on;
+    self.off = off;
+    self.time = time;
+    self.current = current;
 
-  	return self;
+    self.duration = function (dur) { duration = dur; }
+    self.resolution = function (res) { resolution = res; }
+    self.format = function (fmt) { format = fmt; }
+
+    return self;
 
   }
 
